0f5a758b9dbd7367f1f3773e9636cb082383860b
Kohki SHIKATA <indigoworks@gmail.com>
2024-11-22 15:44:58 +0900

    Fix incorrect build setting

--- vite.config.js
+++ vite.config.js
@@ -25,6 +25,28 @@ export default defineConfig({
     assetsDir: 'assets',
     rollupOptions: {
       input: ['./src/pages/**/*.html'],
+      output: {
+        entryFileNames: `assets/css/[name].css`,
+        chunkFileNames: `assets/css/[name].css`,
+        assetFileNames: (assetInfo) => {
+          const originalPath = assetInfo.name
+          const relativePath = path.relative('src/assets', originalPath)
+
+          // CSSファイルは assets/css に配置
+          if (/\.css$/.test(assetInfo.name)) {
+            return `assets/css/${path.basename(assetInfo.name)}`
+          }
+
+          // 画像ファイルは assets/img に配置
+          if (/\.(png|jpe?g|svg|gif|tiff|bmp|ico)$/.test(assetInfo.name)) {
+            // 'src/assets/img' 以下の相対パスを保持
+            return `assets/img/${originalPath}`
+          }
+
+          // その他のファイルは assets フォルダ内に配置
+          return `assets/${relativePath}`
+        },
+      },
     },
   },
 })
