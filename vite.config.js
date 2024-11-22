1020bdfa4af81b7de71359ab965df5d41645be25
Kohki SHIKATA <indigoworks@gmail.com>
2024-11-22 15:24:40 +0900

    Fix conflict

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,30 @@
 import { defineConfig } from 'vite'
+import vituum from 'vituum'
+import posthtml from '@vituum/vite-plugin-posthtml'
 
 export default defineConfig({
+  plugins: [
+    vituum({
+      imports: {
+        filenamePattern: {
+          '+.css': [],
+          '+.scss': 'src/assets/css',
+        },
+      },
+    }),
+    posthtml({
+      root: './src',
+    }),
+  ],
   build: {
-    lib: {
-      entry: './src/assets/the-wheels.scss',
-      name: 'TheWheels',
-      fileName: (format) => `the-wheels.${format}.css`,
-    },
+    // lib: {
+    //   entry: './src/assets/the-wheels.scss',
+    //   name: 'TheWheels',
+    //   fileName: (format) => `the-wheels.${format}.css`,
+    // },
+    assetsDir: 'assets',
     rollupOptions: {
-      external: [], // 外部ライブラリがあればここに追加
+      input: ['./src/pages/**/*.html'],
     },
-  },
-  server: {
-    open: '/src/index.html', // 開発サーバー起動時にこのHTMLファイルを表示
   },
 })
