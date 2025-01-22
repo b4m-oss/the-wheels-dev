91638de608362e346e3fee4682e1d33799e758e7
Kohki SHIKATA <indigoworks@gmail.com>
2025-01-23 02:30:37 +0900

    refs #209 work around, but still some things must be resolved left

--- dev/src/pages/kitchen-sink/index.vue
+++ dev/src/pages/kitchen-sink/index.vue
@@ -1,5 +1,6 @@
 <script lang="ts" setup>
 import KitchenSinkNavigation from "~/components/site/container/KitchenSinkNavigation.vue";
+import PageTextTitle from "~/components/system/atoms/PageTextTitle.vue";
 import ContentSection from "~/components/system/container/ContentSection.vue";
 import ButtonContent from "~/components/site/content/01_Button.vue";
 import RadioGroupContent from "~/components/site/content/02_RadioGroup.vue";
@@ -14,22 +15,24 @@ import Formula from "~/components/site/content/Formula.vue";
         <KitchenSinkNavigation />
       </template>
       <template #content>
-        <article slot="content">
-          <h1>Kitchen Sink</h1>
-          <ButtonContent />
-          <RadioGroupContent />
-          <AccordionContent />
-          <ModalContent />
-          <ContentSection
-            title-tag="h2"
-            title="Complex input form"
-            id="complex-input-form"
-          >
-            <form>
-              <Formula />
-            </form>
-          </ContentSection>
-        </article>
+        <div class="twls-layout">
+          <article>
+            <PageTextTitle>Kitchen Sink</PageTextTitle>
+            <ButtonContent />
+            <RadioGroupContent />
+            <AccordionContent />
+            <ModalContent />
+            <ContentSection
+              title-tag="h2"
+              title="Complex input form"
+              id="complex-input-form"
+            >
+              <form>
+                <Formula />
+              </form>
+            </ContentSection>
+          </article>
+        </div>
       </template>
     </NuxtLayout>
   </div>
