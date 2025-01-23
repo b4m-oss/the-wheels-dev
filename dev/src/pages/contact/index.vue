1ff6c08772412068699233617513b4939141cd6b
Kohki SHIKATA <indigoworks@gmail.com>
2025-01-23 13:47:37 +0900

    WIP: refs #198

--- dev/src/pages/contact/index.vue
+++ dev/src/pages/contact/index.vue
@@ -1,6 +1,7 @@
 <script lang="ts" setup>
 import PrivacyPolicy from '~/components/site/elements/PrivacyPolicy.vue';
 import TOC from '~/components/site/elements/TOC.vue';
+import MessagePanel from '~/components/system/elements/MessagePanel.vue';
 import StepNavList from '~/components/system/lists/StepNavList.vue';
 import Checkbox from '~/components/system/atoms/form/Checkbox.vue';
 import Radio from '~/components/system/atoms/form/Radio.vue';
@@ -29,10 +30,12 @@ import Radio from '~/components/system/atoms/form/Radio.vue';
             <Radio name="contact-agree-policy" :required="true" value="営業・協業のご提案">営業・協業のご提案</Radio>
             <Radio name="contact-agree-policy" :required="true" value="その他">その他</Radio>
           </div>
-          <div class="twls-alert">
-            <p>営業・協業が目的の方は<strong>必ず</strong>「営業・協業のご提案」からご連絡ください。</p>
-            <p>詳しくは、<a href="#">サイト利用規約</a>をご覧ください。</p>
-          </div>
+          <MessagePanel html-class="twls-state_alert">
+            <template #content>
+              <p>営業・協業が目的の方は<strong>必ず「営業・協業のご提案」</strong>からご連絡ください。</p>
+              <p>詳しくは、<a href="#">サイト利用規約</a>をご覧ください。</p>
+            </template>
+          </MessagePanel>
         </fieldset>
         <fieldset>
           <legend>ご検討内容について</legend>
@@ -43,25 +46,27 @@ import Radio from '~/components/system/atoms/form/Radio.vue';
             <Checkbox name="contact-customer-service-type[]" :required="false" value="Webサイト・ランディングページ">その他</Checkbox>
           </div>
         </fieldset>
-        <label class="twls-select">
-          <span class="twls-label-text">ご予算</span>
-          <select name="contact-customer-budget" required>
-            <option value="" selected hidden>選択してください</option>
-            <option value="5万円~25万円">5万円~25万円</option>
-            <option value="26万円~100万円">26万円~100万円</option>
-            <option value="101万円~250万円">101万円~250万円</option>
-            <option value="251万円~500万円">250万円~500万円</option>
-            <option value="501万円~1,000万円">501万円~1,000万円</option>
-            <option value="1,000万円以上">1,000万円以上</option>
-            <option value="1,000万円以上">予算未設定</option>
-          </select>
-        </label>
-        <div class="twls-input-field-group">
-          <label class="twls-input">
-            <span class="twls-label-text">ご検討内容の詳細についてお知らせください</span>
-            <textarea name="contact-customer-detail" maxlength="3000" required></textarea>
+        <fieldset>
+          <label class="twls-select">
+            <span class="twls-label-text">ご予算</span>
+            <select name="contact-customer-budget" required>
+              <option value="" selected hidden>選択してください</option>
+              <option value="5万円~25万円">5万円~25万円</option>
+              <option value="26万円~100万円">26万円~100万円</option>
+              <option value="101万円~250万円">101万円~250万円</option>
+              <option value="251万円~500万円">250万円~500万円</option>
+              <option value="501万円~1,000万円">501万円~1,000万円</option>
+              <option value="1,000万円以上">1,000万円以上</option>
+              <option value="1,000万円以上">予算未設定</option>
+            </select>
           </label>
-        </div>
+          <div class="twls-input-field-group">
+            <label class="twls-input">
+              <span class="twls-label-text">ご検討内容の詳細についてお知らせください</span>
+              <textarea name="contact-customer-detail" maxlength="3000" required></textarea>
+            </label>
+          </div>
+        </fieldset>
         <fieldset>
           <legend>お客様情報をご記載ください</legend>
           <div class="twls-input-field-group">
@@ -78,23 +83,25 @@ import Radio from '~/components/system/atoms/form/Radio.vue';
           </div>
           <label>
             <span class="twls-label-text">組織種別</span>
-            <div class="twls-input-field-messages">
-              <p>選択肢については、以下の要領でお願いいたします</p>
-              <dl>
-                <div>
-                  <dt>法人</dt>
-                  <dd>会社組織や、公共団体などの組織で、事業としてご発注の方</dd>
-                </div>
-                <div>
-                  <dt>個人事業主</dt>
-                  <dd>個人事業主（フリーランス・自営業）の方が、ご自身の事業としてご発注の方</dd>
-                </div>
-                <div>
-                  <dt>個人</dt>
-                  <dd>プライベートでのご利用においてご発注の方</dd>
-                </div>
-              </dl>
-            </div>
+            <MessagePanel html-class="">
+              <template #header>選択肢については、以下の要領でお願いいたします</template>
+              <template #content>
+                <dl>
+                  <div>
+                    <dt>法人</dt>
+                    <dd>会社組織や、公共団体などの組織で、事業としてご発注の方</dd>
+                  </div>
+                  <div>
+                    <dt>個人事業主</dt>
+                    <dd>個人事業主（フリーランス・自営業）の方が、ご自身の事業としてご発注の方</dd>
+                  </div>
+                  <div>
+                    <dt>個人</dt>
+                    <dd>プライベートでのご利用においてご発注の方</dd>
+                  </div>
+                </dl>
+              </template>
+            </MessagePanel>
             <select name="contact-user-type">
               <option value="" selected hidden required>選択してください</option>
               <option value="法人">法人</option>
