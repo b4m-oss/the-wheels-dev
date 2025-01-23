9c20dba33933887af12ec480c44399276916c9b4
Kohki SHIKATA <indigoworks@gmail.com>
2025-01-23 13:26:59 +0900

    refs #212 Add message panels

---
+++ dev/src/components/system/elements/MessagePanel.vue
@@ -0,0 +1,16 @@
+<script lang="ts" setup>
+const props = defineProps<{
+  htmlClass: string;
+}>()
+</script>
+
+<template>
+  <div :class="props.htmlClass ? 'twls-message-panel ' + props.htmlClass : 'twls-message-panel'">
+    <header class="twls-message-panel_header">
+      <slot name="header"></slot>
+    </header>
+    <div class="twls-message-panel_content">
+      <slot name="content"></slot>
+    </div>
+  </div>
+</template>
