33b479081b0c242e34dad274550bbd19e196ee7b
Kohki SHIKATA <indigoworks@gmail.com>
2025-01-23 16:31:28 +0900

    WIP: refs #198 Polish Message panel, update spacing when header is empty

--- dev/src/components/system/elements/MessagePanel.vue
+++ dev/src/components/system/elements/MessagePanel.vue
@@ -1,12 +1,14 @@
 <script lang="ts" setup>
+import { onMounted, useSlots } from 'vue';
 const props = defineProps<{
   htmlClass: string;
 }>()
+
 </script>
 
 <template>
   <div :class="props.htmlClass ? 'twls-message-panel ' + props.htmlClass : 'twls-message-panel'">
-    <header class="twls-message-panel_header">
+    <header class="twls-message-panel_header" v-if="$slots.header">
       <slot name="header"></slot>
     </header>
     <div class="twls-message-panel_content">
