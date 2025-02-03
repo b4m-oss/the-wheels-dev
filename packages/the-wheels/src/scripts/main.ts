f0e33baba17fbed4861824fbfb51a96b73a2c133
Kohki SHIKATA <indigoworks@gmail.com>
2025-02-03 21:14:17 +0900

    WIP: refs #218 Add Icons

--- packages/the-wheels/src/scripts/main.ts
+++ packages/the-wheels/src/scripts/main.ts
@@ -9,6 +9,14 @@ import TwlsRadioGroupItem from "./lib/components/atoms/TwlsRadioGroupItem";
 import TwlsModal from "./lib/components/modules/TwlsModal";
 import TwlsAccordion from "./lib/components/modules/TwlsAccordion";
 import TwlsAccordionContainer from "./lib/components/lists/TwlsAccordionContainer";
+import TwlsMessagePanel from "./lib/components/elements/TwlsMessagePanel";
+import TwlsIconAlertTriangle from "./lib/components/icons/TwlsIconAlertTriangle";
+import TwlsIconPC from "./lib/components/icons/TwlsIconPC";
+import TwlsIconSun from "./lib/components/icons/TwlsIconSun";
+import TwlsIconMoon from "./lib/components/icons/TwlsIconMoon";
+import TwlsIconLock from "./lib/components/icons/TwlsIconLock";
+import TwlsIconArrow from "./lib/components/icons/TwlsIconArrow";
+import TwlsIconTriangle from "./lib/components/icons/TwlsIconTriangle";
 
 export { 
   TwlsCardButton, 
@@ -20,5 +28,13 @@ export {
   TwlsRadioGroupItem,
   TwlsModal,
   TwlsAccordion,
-  TwlsAccordionContainer 
+  TwlsAccordionContainer,
+  TwlsMessagePanel,
+  TwlsIconAlertTriangle,
+  TwlsIconPC,
+  TwlsIconSun,
+  TwlsIconMoon,
+  TwlsIconLock,
+  TwlsIconArrow,
+  TwlsIconTriangle
 };
