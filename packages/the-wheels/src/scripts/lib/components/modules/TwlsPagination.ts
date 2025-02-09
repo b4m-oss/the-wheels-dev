5fa4890dfd01dc2ba4cfcc40f87f63aff0f0899c
Kohki SHIKATA <4133263+kohki-shikata@users.noreply.github.com>
2025-02-09 04:06:20 +0000

    WIP: refs #208 Pagination skelton and style is work around

--- packages/the-wheels/src/scripts/lib/components/modules/TwlsPagination.ts
+++ packages/the-wheels/src/scripts/lib/components/modules/TwlsPagination.ts
@@ -8,22 +8,79 @@ class TwlsPagination extends HTMLElement {
   render() {
     this.innerHTML = `
       <nav class="pagination">
-        <div class="pager-bubble"></div>
         <div class="pager-container">
-          <button type="button" class="pager-button-oldest">oldest</button>
-          <button type="button" class="pager-button-prev">prev</button>
-          <div class="pager-counter">
-            <span class="pager-count-current">1</span>
-            <span class="pager-count-divider">/</span>
-            <span class="pager-count-total">48</span>
-          </div>
-          <button type="button" class="pager-button-next">>next</button>
-          <button type="button" class="pager-button-latest">latest</button>
+          <button type="button" class="pager-button-oldest">
+            <twls-icon-chevron-end width="12" height="12" rotate="180"></twls-icon-chevron-end>
+          </button>
+          <button type="button" class="pager-button-prev">
+            <twls-icon-chevron width="12" height="12" rotate="180"></twls-icon-chevron>
+          </button>
+          <twls-dropdown>
+            <button class="pager-counter trigger">
+              <span class="pager-count-current">3ページ目</span>
+              <span class="pager-count-divider">/</span>
+              <span class="pager-count-total">全48ページ</span>
+            </button>
+            <div class="content">
+              <ul>
+                <li><a href="">1</a></li>
+                <li><a href="">2</a></li>
+                <li><a href="">3</a></li>
+                <li><a href="">4</a></li>
+                <li><a href="">5</a></li>
+                <li><a href="">6</a></li>
+                <li><a href="">7</a></li>
+                <li><a href="">8</a></li>
+                <li><a href="">9</a></li>
+                <li><a href="">10</a></li>
+                <li><a href="">11</a></li>
+                <li><a href="">12</a></li>
+                <li><a href="">13</a></li>
+                <li><a href="">14</a></li>
+                <li><a href="">15</a></li>
+                <li><a href="">16</a></li>
+                <li><a href="">17</a></li>
+                <li><a href="">18</a></li>
+                <li><a href="">19</a></li>
+                <li><a href="">20</a></li>
+                <li><a href="">21</a></li>
+                <li><a href="">22</a></li>
+                <li><a href="">23</a></li>
+                <li><a href="">24</a></li>
+                <li><a href="">25</a></li>
+                <li><a href="">26</a></li>
+                <li><a href="">27</a></li>
+                <li><a href="">28</a></li>
+                <li><a href="">29</a></li>
+                <li><a href="">30</a></li>
+                <li><a href="">31</a></li>
+                <li><a href="">32</a></li>
+                <li><a href="">33</a></li>
+                <li><a href="">34</a></li>
+                <li><a href="">35</a></li>
+                <li><a href="">36</a></li>
+                <li><a href="">37</a></li>
+                <li><a href="">38</a></li>
+                <li><a href="">39</a></li>
+                <li><a href="">40</a></li>
+                <li><a href="">41</a></li>
+                <li><a href="">42</a></li>
+                <li><a href="">43</a></li>
+              </ul>
+            </div>
+          </twls-dropdown>
+          
+          <button type="button" class="pager-button-next">
+              <twls-icon-chevron width="12" height="12" rotate="0"></twls-icon-chevron>
+          </button>
+          <button type="button" class="pager-button-latest">
+            <twls-icon-chevron-end width="12" height="12" rotate="0"></twls-icon-chevron-end>
+          </button>
         </div>
-        <div class="pager-item-counter>
-          <span class="pager-item-count-current">1~10</span>
+        <div class="pager-item-counter">
+          <span class="pager-item-count-current">31~40件</span>
           <span class="pager-item-count-divider">/</span>
-          <span class="pager-item-count-total">476</span>
+          <span class="pager-item-count-total">全476件</span>
         </div>
       </nav>
     `
